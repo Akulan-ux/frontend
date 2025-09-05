69aa4ed1758d5090ab3e19ccbc293958b68a4c81
santosh.chanda <santosh.chanda@dristia.com>
2025-09-05 12:21:32 +0530

    Added everything

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  VITE_BASE_PATH: process.env.VITE_BASE_PATH || '/frontend'
 })
